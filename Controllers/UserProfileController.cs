e8761783dd0ef43c88a352bdfeffb53377e25005
luqmanzulfiqarAhmed <40490385+luqmanzulfiqarAhmed@users.noreply.github.com>
2020-12-05 19:41:22 +0500

    user profile controller

---
+++ Controllers/UserProfileController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EF_DietaNoDietaApi.Model;
+using EF_DietaNoDietaApi.MySql;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EF_DietaNoDietaApi.Controllers
+{
+    [Route("api/UserProfile")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly MySqlDbContext dbContext;
+
+        public UserProfileController(MySqlDbContext context, IConfiguration configuration)
+        {
+
+            dbContext = context;
+            _config = configuration;
+        }
+        [HttpGet]
+        [Route("getProfiles")]
+        public async Task<IActionResult> getProfiles([FromQuery] int item){
+            IQueryable<Model.UserModel> result=null;
+            if (item == 0)//get all users
+            {
+                result = dbContext.Users.Where(p => p.UserRole == UserRoles.User);
+                return StatusCode(StatusCodes.Status200OK, result);
+            }
+            else if (item == 1)//get all verified users
+            {
+                result = dbContext.Users.Where(p => p.UserRole == UserRoles.User && p.isVeified == "true");
+                return StatusCode(StatusCodes.Status200OK, result);
+            }
+            else if (item == 2)//get all blocked users
+            {
+                result = dbContext.Users.Where(p => p.UserRole == UserRoles.User && p.isVeified == "false");
+                return StatusCode(StatusCodes.Status200OK, result);
+            }
+            else if (item == 3)//get all not verified users
+            {
+                result = dbContext.Users.Where(p => p.UserRole == UserRoles.User && p.isVeified == "waiting");
+                return StatusCode(StatusCodes.Status200OK, result);
+            }
+            return StatusCode(StatusCodes.Status406NotAcceptable, new {Message = "type of user not found" });
+
+        }
+
+        [HttpPut]
+        [Route("updateRequest")]
+        //authorize this method for admin only
+        public async Task<IActionResult> updateRequest([FromBody] UserModel user,[FromQuery] String request) {
+            var found = dbContext.Users.FindAsync(user.email);
+            UserModel result = found.Result;
+            if (user.password != result.password) {
+                return StatusCode(StatusCodes.Status401Unauthorized, new Response { Status = "401", Message = "Incorect Password" });
+            }
+            if (result == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new Response { Status = "404", Message = "User with this email not found" });
+            }
+            result.isVeified = request;
+            dbContext.Users.Update(result);
+            await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK,result);
+        }
+
+        [HttpPut]
+        [Route("updateProfile")]        
+        public async Task<IActionResult> updateProfile([FromBody] UserModel user)
+        {
+            //var found = dbContext.Users.FindAsync(user.email);
+            //UserModel result = found.Result;
+            //if (user.password != result.password)
+            //{
+            //    return StatusCode(StatusCodes.Status401Unauthorized, new Response { Status = "401", Message = "Incorect Password" });
+            //}
+            //if (result == null)
+            //{
+            //    return StatusCode(StatusCodes.Status404NotFound, new Response { Status = "404", Message = "User with this email not found" });
+            //}
+            //String verify = result.isVeified;
+            //user.isVeified = verify;
+            //dbContext.Users.Update(user);
+            //await dbContext.SaveChangesAsync();
+
+            try
+            {
+                var local = dbContext.Set<UserModel>()
+                                     .Local
+                                     .FirstOrDefault(entry => entry.email.Equals(user.email));                
+                // check if local is not null 
+                if (local != null)
+                {
+                    // detach
+                    dbContext.Entry(local).State = EntityState.Detached;                    
+                }
+                // set Modified flag in your entry
+                dbContext.Entry(user).State = EntityState.Modified;                
+                // save 
+                dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, new { Message = "Profile Updated Successfully!",Profile= user });
+            }
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status404NotFound, new { Message = "Email not found!" });
+            }
+        }
+    }
+}
